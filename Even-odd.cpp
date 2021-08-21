011e43e9470096b065cdc95af6cb1ac081b7f194
Asif tyagi <59314914+Asiftyagi-kiet@users.noreply.github.com>
2021-08-22 00:03:42 +0530

    Add files via upload

---
+++ Even-odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the number"<<endl;
+    int x;
+    cin>>x;
+    
+    if(x%2==0)
+    {
+        cout<<"Even Number"<<endl;
+    }
+    else{
+        cout<<"Odd Number"<<endl;
+    }
+
+}
